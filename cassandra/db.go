9fde52518c14ce91a0ae14215b6170fa5d66e67f
aarshi <aarshi.arora@tokopedia.com>
2020-03-24 05:23:47 +0530

    Add Cassandra configuration

---
+++ cassandra/db.go
@@ -0,0 +1,19 @@
+package cassandra
+
+import (
+	"fmt"
+	"github.com/gocql/gocql"
+)
+
+var Session *gocql.Session
+func init() {
+	var err error
+	cluster := gocql.NewCluster("127.0.0.1")
+	cluster.Keyspace = "dev"
+	Session, err = cluster.CreateSession()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("cassandra init done")
+	return
+}
