c4c7d4f198b960cbbfa582aeb44bf1a2bb2f57cd
aarshi <aarshi.arora@tokopedia.com>
2020-03-24 13:03:29 +0530

    Expose routing layer in main application

--- app.go
+++ app.go
@@ -1,30 +1,20 @@
 package main
 
 import (
-	"encoding/json"
 	"github.com/gorilla/mux"
 	"github.com/poc/url-shortner/cassandra"
-	"github.com/poc/url-shortner/handler"
+	routes "github.com/poc/url-shortner/router"
 	"log"
 	"net/http"
 )
 
-type heartbeatResponse struct {
-	Status string `json:"status"`
-	Code   int    `json:"code"`
-}
-
-func heartbeat(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(heartbeatResponse{Status: "OK", Code: 200})
-}
 func main() {
 	CassandraSession := cassandra.Session
 	defer CassandraSession.Close()
 	router := mux.NewRouter().StrictSlash(true)
-	//router.HandleFunc("/", heartbeat)
-//	router.HandleFunc("/create", handler.GetHash)
-	router.HandleFunc("/toko/{pattern}", handler.RedirectURL)
+	routes.HandleHttpRequest(router)
 	log.Println("Application started")
 	//cassandra.CreateKeys()
 	log.Fatal(http.ListenAndServe(":8090", router))
+	log.Println("Application started")
 }
