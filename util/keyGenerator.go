d393434bf2a799d2f20b8622803309a1bbaafb48
aarshi <aarshi.arora@tokopedia.com>
2020-03-24 13:56:55 +0530

    Remove fmt ,use log and add go docs

--- util/keyGenerator.go
+++ util/keyGenerator.go
@@ -2,31 +2,39 @@ package util
 
 import (
 	"encoding/base64"
-	"fmt"
+	"log"
 	"strconv"
 	"time"
 )
 
 var KeySet []string
 
-func GetKeys()  {
+/**
+Generates Unique keys
+*/
+func GetKeys() {
 	finished := make(chan [] string)
 	latest := time.Now().Unix()
+	/**
+	use go routine to generate keys
+	*/
 	go generateKey(finished, latest)
-	fmt.Println("processing")
+	log.Println("processing")
 	<-finished
 	return
 }
 
-func generateKey(finished chan [] string , latest int64) {
+/**
+Generates 130K unique keys in a second
+*/
+func generateKey(finished chan [] string, latest int64) {
 	var i int64
-	fmt.Println("worker started")
+	log.Println("worker started")
 	for i = 0000000000000000; i < 130000; i++ {
 		str := strconv.FormatInt(latest+i, 31)
 		encoded := base64.StdEncoding.EncodeToString([]byte(str))
 		KeySet = append(KeySet, encoded)
-		fmt.Println(encoded)
 	}
-	fmt.Println("worker processed")
+	log.Println("worker processed")
 	finished <- KeySet
 }
