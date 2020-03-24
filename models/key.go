ab62b2ed3727b0547e311c01cc4f439905202b26
aarshi <aarshi.arora@tokopedia.com>
2020-03-24 13:48:11 +0530

    Add go docs in key model

--- models/key.go
+++ models/key.go
@@ -1,10 +1,15 @@
 package models
 
-
+/**
+Enumerates available keys
+*/
 type Key struct {
 	Key string `json:"key" db:"id,pk"`
 }
 
-type UsedKey struct{
+/**
+Enumerates used keys
+*/
+type UsedKey struct {
 	UsedKey string `json:"key" db:"id,pk"`
 }
