ffd08683bf6dbbc914eb75a5ba4365b42bdfef5b
aarshi <aarshi.arora@tokopedia.com>
2020-03-24 13:46:49 +0530

    Enhance error handling and loggig in url repository

--- repository/url.go
+++ repository/url.go
@@ -1,31 +1,44 @@
 package repository
 
 import (
-	"fmt"
+	"errors"
+	"github.com/gocql/gocql"
 	"github.com/poc/url-shortner/cassandra"
 	"github.com/poc/url-shortner/models"
+	"log"
 )
 
-func CreatUrl(url models.Url) {
-	fmt.Println(" **** Creating new url ****\n")
+/***
+Create Mapping of URL and hash in database
+*/
+func CreatUrl(url models.Url) (err error) {
+	log.Println(" **** Creating new url ****\n")
 	if err := cassandra.Session.Query("INSERT INTO url(hash, creation_date, expiration_date, original_url) VALUES(?, ?, ?, ?)",
 		url.Hash, url.CreationDate, url.ExpirationDate, url.OriginalUrl).Exec(); err != nil {
-		fmt.Println("Error while inserting  url")
-		fmt.Println(err)
-
+		message := "Error while inserting  url " + error.Error(err)
+		log.Println(message)
+		err = errors.New(message)
 	}
+	return
 }
 
-func GetOriginalURL(key string) []models.Url {
-	fmt.Println(" **** Fetching url ****\n")
+/**
+Fetch URL for hash
+*/
+func GetOriginalURL(key string) (url []models.Url, err error) {
+	log.Println(" **** Fetching url ****\n")
 	m := map[string]interface{}{}
-	var url []models.Url
-	urlQuery := cassandra.Session.Query("Select * from url where hash= ?", key).Iter()
+	var urlQuery *gocql.Iter
+	urlQuery = cassandra.Session.Query("Select * from url where hash= ?", key).Iter()
+	urlQuery.Close()
+	if urlQuery.NumRows() <= 0 {
+		err = errors.New("No mapping exists for key: " + key)
+		return
+	}
 	for urlQuery.MapScan(m) {
 		url = append(url, models.Url{
 			OriginalUrl: m["original_url"].(string),
 		})
 	}
-	fmt.Println("Error while inserting  url")
-	return url
+	return
 }
